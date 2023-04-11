95a8e18bd0d833c299cffa3d0afd5d9e173228ec
aidar <aidar.galeeff@yandex.ru>
2023-04-11 18:09:15 +0500

    Add PerfectHealthController and view

---
+++ Automarket/Controllers/PerfectHealthController.cs
@@ -0,0 +1,25 @@
+﻿using Automarket.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Automarket.Controllers;
+
+public class PerfectHealthController : Controller
+{
+    private readonly IPerfectHealthService _health;
+
+    public PerfectHealthController(IPerfectHealthService healthService)
+    {
+        _health = healthService;
+    }
+    
+    [HttpGet]
+    public IActionResult GetHealths()
+    {
+        var response = _health.GetHealths();
+        if (response.StatusCode == Domain.Enum.StatusCode.OK)
+        {
+            return View(response.Data);   
+        }
+        return View("Error", $"{response.Description}");
+    }
+}
