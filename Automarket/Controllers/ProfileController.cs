138c2fe1fe1c823692f8ea5a5aba79f8120f1601
aidar <aidar.galeeff@yandex.ru>
2023-05-14 17:42:58 +0500

    Tried add some shit

--- Automarket/Controllers/ProfileController.cs
+++ Automarket/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Automarket.DAL.Interfaces;
 using Automarket.Domain.Entity;
@@ -14,6 +15,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
+using TeleSharp.TL;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
 
 namespace Automarket.Controllers
 {
@@ -88,6 +92,46 @@ namespace Automarket.Controllers
         [HttpGet]
         public async Task<IActionResult> Detail(string userName)
         {
+            
+            // var client = new TelegramClient(22373081, "f7f72fce3ca7da530daaf6cca3db280e");
+            // await client.ConnectAsync();
+            //
+            // // var result = await client.GetContactsAsync();
+            //
+            // var dialogsResult = (TLDialogs) await client.GetUserDialogsAsync();
+            // var users = dialogsResult.Users.OfType<TLUser>();
+            // var bot = users.FirstOrDefault(x => x.Username == "chatgpt_tgm_bot");
+            // var peer = new TLInputPeerUser { UserId = bot.Id, AccessHash = bot.AccessHash.Value };
+            // var result = await client.SendMessageAsync(peer, "message");
+            
+            // var user = result.Users
+            //     .OfType<TLUser>()
+            //     .FirstOrDefault(x => x.Username == "GPT4Telegrambot");
+            //
+            // if (user == null)
+            // {
+            //     throw new System.Exception("Number was not found in Contacts List of user: " + "79632354724");
+            // }
+            //
+            // await client.SendTypingAsync(new TLInputPeerUser() { UserId = user.Id });
+            // Thread.Sleep(3000);
+            // await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, "TEST");
+
+            // var hash = await client.SendCodeRequestAsync("79632361218");
+            // var code = "68150"; // you can change code in debugger
+            // var user = await client.MakeAuthAsync("79632361218", hash, code);
+            //
+            //get user dialogs
+            // var dialogs = (TLDialogs) await client.GetUserDialogsAsync();
+            //
+            // //find channel by title
+            // var chat = dialogs.Chats
+            //     .OfType<TLChannel>()
+            //     .FirstOrDefault(c => c.Title == "@GPT4Telegrambot");
+            //
+            // //send message
+            // await client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = chat.Id, AccessHash = chat.AccessHash.Value }, "TEST MSG");
+            
             // var userName = User.Identity.Name;
             var response = await _profileService.GetProfile(userName);
             if (response.StatusCode == Domain.Enum.StatusCode.OK)
